af8af631f720d071f4b292502558f0d3cf80d1b2
Md Amirozzaman <amirozzaman@ticonsys.com>
2023-11-30 16:02:31 +0600

    initialized commit

--- app/root.tsx
+++ app/root.tsx
@@ -8,8 +8,9 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-
+import styles from "./globals.css";
 export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import * as React from "react";
+
 import type { MetaFunction } from "@remix-run/node";
+import { Button } from "components/ui/button";
+import { Calendar } from "components/ui/calendar";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,34 +14,20 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <Button>Click Me</Button>
+        <label>Date:</label>
+        <span>{date?.toDateString()}</span>
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-md border"
+        />
+      </div>
+    </main>
   );
 }
